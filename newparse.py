5932e5618c62cfc96fa8252ffb1a34c62571fbfb
Maximilian Burkhardt <maxb@rescomp.berkeley.edu>
2014-01-13 15:44:50 -0800

    Added plugin filtering mutator. Made text output respect numeric-ids.

--- newparse.py
+++ newparse.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
     # process options which set flags for other modules
     if args.numeric_ids:
         environ['numeric_ids'] = True
+    else:
+        environ['numeric_ids'] = False
 
     # process mutators, according to which ones were selected
     if args.condense_java:
@@ -21,12 +23,17 @@ if __name__ == '__main__':
     if args.select_adobe:
         from mutators import select_adobe
         select_adobe.mutate(csv_data, args.select_adobe, environ)
-    if args.level:
+    # level and plugin_list don't really make sense together,
+    # so we ignore level if plugin_list is present
+    if args.level and args.plugin_list == None:
         from mutators import level
         level.mutate(csv_data, args.level, environ)
     if args.hostname_regex:
         from mutators import hostname_regex
         hostname_regex.mutate(csv_data, args.hostname_regex, environ)
+    if args.plugin_list:
+        from mutators import plugin_list
+        plugin_list.mutate(csv_data, args.plugin_list, environ)
 
     # process output modules
     from output import text
--- output/text.py
+++ output/text.py
@@ -1,7 +1,12 @@
 from scandata import ScanData
 def output(csv_data, environ):
     for vuln,hosts in csv_data.vuln_to_hosts.iteritems():
-        print "=====", csv_data.id_to_name[vuln], "====="
+        print "=====", 
+        if environ['numeric_ids']:
+            print vuln,
+        else:
+            print csv_data.id_to_name[vuln], 
+        print "====="
         for host in hosts:
             print host, "\t\t", csv_data.host_to_ip[host]
         print " "
