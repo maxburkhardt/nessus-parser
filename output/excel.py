8b9f1f3e22ef7bd19031add8bc75cad030a8b4b2
Maximilian Burkhardt <maxb@rescomp.berkeley.edu>
2014-01-22 16:22:33 -0800

    Simplified Excel output, also made it more functional.

--- output/excel.py
+++ output/excel.py
@@ -3,52 +3,35 @@ def output(csv_data, excel_file, environment):
     print "Making an excel file in", excel_file, "!"
     book = Workbook()
     sheet = book.add_sheet("Nessus Vulnerabilities", cell_overwrite_ok=True)        #creating sheet + fonts
+    sheet.header_str = ("&Lleft\nPage &P of &N")
     default_style = easyxf("font: name Calibri, height 240;")
-    header_style = easyxf("font: name Calibri, height 240; pattern: pattern diamonds, fore_colour pale_blue;")
+    header_style = easyxf("font: name Calibri, height 240; pattern: pattern diamonds, fore_colour pale_blue; align: wrap 1;")
     header = sheet.row(0)
     header.write(0, "Hostname", header_style)                    #headers
     header.write(1, "IP", header_style)
-    header.write(2, "Name", header_style)
-    for i in range(len(csv_data.vuln_to_hosts.keys())):
-        header.write(i + 3, " ", header_style)
-    header.height = 300
+
+    vuln_names = map(lambda x: csv_data.id_to_name[x], csv_data.vuln_to_hosts.keys())
+    vuln_hosts = csv_data.vuln_to_hosts.values()
+
+    for i in range(len(vuln_names)):
+        header.write(i + 2, vuln_names[i], header_style)
+
+    header.height = 900
     row_count = 1
     col_count = 2
     sheet.col(0).width = 10000
     sheet.col(1).width = 5000
     sheet.col(2).width = 10000
-    if not environment['numeric_ids']:                       # we're sorting by name, not plugin id
-        collvulns = {}
-        for vuln,name in csv_data.id_to_name.iteritems():              # Go through each vuln in id_to_name, collate ones to be collated
-            if name[:2] == "MS":                # This should really be refactored to pull from some sort of file in the future
-                name = "Needs Windows Updates"
-            elif name.find("<") != -1:
-                name = "Outdated " + name.split("<")[0]
-            collvulns[vuln] = name
-        collatedvulns = collvulns
-        #for vuln,name in collvulns.iteritems():     # TODO: look through the vulns again to find tags for outdated software that don't have < signs
-        #    if name.startswith("Outdated "):
-        #        searchname = name.split("Outdated ")[1]
-        sortedvulns = sorted(list(set(collatedvulns.values())))                #make a list of the sorted + collated vulnerabilities
-        for vuln in sortedvulns:                                    #setting widths for vulnerability names
-            sheet.col(col_count).width = 300 * len(vuln)
-            col_count += 1
-    else:
-        sortedvulns = sorted(csv_data.vuln_to_hosts.keys())
-        for vuln in sortedvulns:
-            sheet.col(col_count).width = 300 * (len(str(vuln)) + 2)
-            col_count += 1
+    for vuln in vuln_names:
+        sheet.col(col_count).width = 150 * (len(str(vuln)) + 2)
+        col_count += 1
     for host in csv_data.host_to_vulns.keys():
         row = sheet.row(row_count)
         row.write(0, host, default_style)
         row.write(1, csv_data.host_to_ip[host], default_style)
-        if not environment['numeric_ids']:                                   # pull from alphabetical list, not plugin list
-            for vuln in csv_data.host_to_vulns[host]:
-                row.write(sortedvulns.index(collatedvulns[vuln]) + 2, collatedvulns[vuln], easyxf("font: name Calibri, height 240;"
-                    "pattern: pattern solid, fore_colour red;"))
-        else:
-            for vuln in csv_data.host_to_vulns[host]:
-                row.write(sortedvulns.index(vuln) + 2, vuln, easyxf("font: name Calibri, height 240;"
+        for i in range(len(vuln_names)):
+            if host in vuln_hosts[i]:
+                row.write(i + 2, " ", easyxf("font: name Calibri, height 240;"
                     "pattern: pattern solid, fore_colour red;"))
         row.height = 300
         row_count += 1
