77f00946defb925aa12767b7469ce2abfc3983bf
Maximilian Burkhardt <squid219@gmail.com>
2014-03-21 17:21:54 -0700

    Dashes were still causing replies to happen, so switching to tildes.

--- output/text.py
+++ output/text.py
@@ -1,11 +1,11 @@
 def output(csv_data, environ):
     for vuln,hosts in csv_data.vuln_to_hosts.iteritems():
-        print "-----", 
+        print "~~~~~", 
         if environ['numeric_ids']:
             print vuln,
         else:
             print csv_data.id_to_name[vuln], 
-        print "-----"
+        print "~~~~~"
         for host in hosts:
             print host, "\t\t", csv_data.host_to_ip[host]
         print " "
