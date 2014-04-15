41b3e3aeedb0bec7c937ed40651a591b8b12e111
Arlan Jaska <ajaska@berkeley.edu>
2014-04-15 10:13:50 -0700

    Bugfix (hosts in exceptions not in scan data)

--- mutators/exceptions.py
+++ mutators/exceptions.py
@@ -6,6 +6,12 @@ def mutate(csv_data, exceptions_file, environ):
              hostname, id = exception.split()
           except ValueError:
              continue
+          # commented lines:
+          if hostname[0] = '#':
+             continue
+          # don't do anything if we don't have matches for the hostname
+          if not hostname in csv_data.host_to_vulns:
+             continue
           csv_data.host_to_vulns[hostname] = filter( lambda vuln: vuln != id, csv_data.host_to_vulns[hostname] )
           if not csv_data.host_to_vulns[hostname]:
              del csv_data.host_to_vulns[hostname]
